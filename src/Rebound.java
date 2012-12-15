33b743673383b981509ed3b3313ede67a98377fd
binleyyang <ybinley@gmail.com>
2012-12-15 01:04:42 -0500

    updates

--- src/Rebound.java
+++ src/Rebound.java
@@ -1,27 +1,23 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import acm.graphics.*;
-import acm.util.*;
 
 @SuppressWarnings("serial")
 public class Rebound extends JPanel { 
 	
-	private GRect brick;
-	private GOval ball;
 	ImageIcon rectangle;
 	static JLabel paddle;
-	static JLabel ball1;
-	private double vx, vy;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
+	static JPanel frame;
 	
-	public void init() {
+	public Rebound() {
+		
 		setLayout(new BorderLayout());
+		setBackground(Color.white);
 		ActionMap actionMap = getActionMap();
 		int check = JComponent.WHEN_IN_FOCUSED_WINDOW;
 		InputMap inputMap = getInputMap(check);
 		
-		for (Direction direction : Direction.values()) {
+		for (paddleDirection direction : paddleDirection.values()) {
 			inputMap.put(direction.getKeyStroke(), direction.getText());
 			actionMap.put(direction.getText(), new Movement(direction.getText()));
 		}
@@ -32,40 +28,10 @@ public class Rebound extends JPanel {
 		rectangle.setImage(i2);
 		paddle = new JLabel(rectangle);
 		
-		//drawing the ball
-		double x = getWidth()/2 - 10;
-		double y = getHeight()/2 - 10;
-		ball = new GOval(x, y, 10, 10);
-		ball.setFilled(true);
-		add(ball);
-		
-		add(paddle, BorderLayout.SOUTH);
-	}
-	
-	public Rebound() {
-		
-		init();
-		moveBall();
-		
-		//set velocity for the ball's x and y
-		vx = 1;
-		vy = 2;
-	}
-	
-	private void moveBall() {
-		double x = ball1.getX();
-		double y = ball1.getY();
-		
-		if (x == 0 || x == getWidth() - 10)
-			vx = -vx;
-		if (y == 0 || y == getHeight() -10)
-			vy = -vy;
-		
-		ball.move(vx, vy);
-	}
-	
-	private void addBricks() {
-		
+		frame = new JPanel();
+		add(frame, BorderLayout.CENTER);
+		frame.add(paddle, BorderLayout.SOUTH);
+		paddle.setLocation(400, 741);
 	}
 	
 	private class Movement extends AbstractAction {
@@ -78,24 +44,27 @@ public class Rebound extends JPanel {
 		public void actionPerformed(ActionEvent e) {
 			
 			int x = paddle.getX();
-			int y = paddle.getY();
 			String move = e.getActionCommand();
 			
 			if (move.equals("Left"))
 				paddle.setLocation(x-10, 741);
 			if (move.equals("Right"))
 				paddle.setLocation(x+10, 741);
-			
-			repaint();
+			if (move.equals("Space")) {
+				Ball ball = new Ball(frame);
+				ball.start();
+			}
 		}
 	}
+}
 	
-	enum Direction {
+	enum paddleDirection {
 			
 		LEFT("Left", KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0)),
-		RIGHT("Right", KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0));
+		RIGHT("Right", KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0)),
+		SPACE("Space", KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0));
 
-		Direction(String text, KeyStroke keyStroke) {
+		paddleDirection(String text, KeyStroke keyStroke) {
 		    this.name = name;
 		    this.keyStroke = keyStroke;
 		}
@@ -116,4 +85,67 @@ public class Rebound extends JPanel {
 			return name;
 	    }
 	}
-}
+	
+	class Ball extends Thread {
+
+		private int vx = 2, vy = 2, x = 400, y = 400;
+		private JPanel box;
+		
+		public Ball (JPanel m) {
+			box = m;
+		}
+		
+		public void addBall() {
+			Graphics g = box.getGraphics();
+			int a = Rebound.paddle.getX();
+			int b = Rebound.paddle.getY();
+			g.fillOval(a, b, 10, 10);
+			g.dispose();
+		}
+		
+		public void moveBall() {
+			Graphics g = box.getGraphics();
+			g.setXORMode(box.getBackground());
+		    g.fillOval(x, y, 10, 10);
+		    x -= vx;
+		    y -= vy;
+		    Dimension d = box.getSize();
+			    
+		    if (x < 0) {
+		    	x = 0;
+		    	vx = -vx;
+		    }
+			    
+		    if (x + 10 >= d.width) {
+		    	x = d.width - 10;
+		    	vx = -vx;
+		    }
+			    
+		    if (y < 0) {
+		    	y = 0;
+		    	vy = -vy;
+		    }
+			    
+		    if (y + 10 >= d.height) {
+		    	y = d.height - 10;
+		    	vy = -vy;
+			}
+			    
+		    g.fillOval(x, y, 10, 10);
+		    g.dispose();
+		}
+
+		public void run() {
+			try {
+				addBall();
+				for (int i = 1; i > 0; i++) {
+					  moveBall();
+					  sleep(5);
+				}	
+			} 
+			catch (InterruptedException e) {
+				//nothing
+			}
+		}
+	}
+
